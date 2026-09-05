635c0a911c6af00a5ee0ca80f84faed40b53ebe3
tovernator <auke.tegel@protonmail.com>
2026-09-05 15:10:15 +0200

    restructured system config

--- system/default.nix
+++ system/default.nix
@@ -6,12 +6,10 @@
 }:
 {
   imports = [
-    ./locale.nix
-    ./users/cutie.nix
-    ./desktop.nix
-    ./boot.nix
-    ./firewall.nix
-    ./misc.nix
+    ./modules/base
+    ./modules/network
+    ./modules/plover 
+    ./users/cutie.nix 
   ];
 
   nix.settings = {
@@ -21,35 +19,12 @@
     ];
   };
 
-  hardware.bluetooth.enable = true;
-  networking.networkmanager.enable = true;
   networking.hostName = hostname;
-
-  services = {
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
-    };
-
-    gnome.gnome-keyring.enable = true;
-    pulseaudio.enable = false;
-    udisks2.enable = true;
-
-    tuned.enable = true;
-    upower.enable = true;
-
-  };
-
-  security.polkit.enable = true;
 
   environment.systemPackages = with pkgs; [
     helix
     curl
     git
-    libsecret
   ];
 
   programs.bash = {
